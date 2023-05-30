9360677bb546824b67c074f89af87c4697339979
onejuice98 <waterpurifier@khu.ac.kr>
2023-05-30 19:04:28 +0900

    시작하기 전 nest JS 공부

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/**
+ * @ 는 데코레이터다. 클래스 위의 함수이고, 클래스에 함수 기능을 추가할 수 있다. 클래스를 위해 움직인다.
+ * 다른 모듈을 만들 것이면, appModule에 import하여 사용하는것이다. 그게 메인에 있으니깐, 인증이나 다른 것들이 만들면,
+ * 컨트롤러는 url을 가져오고 function을 리턴
+ * 서비스는 비즈니스 로직(function)을 만드는 곳.
+ */
 @Module({
   imports: [],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/**
+ * main.ts 는 꼭 필요하다.
+ * bootstrap은 이름이 바뀌어도 될것이다.
+ * 3000번의 포트 사용
+ * Hello world는 AppModule이 클래스
+ * 모든 것의 시작, AppModule은 하나의 앱이다. // 인증을 담당하는 어플리케이션이 있다면 user Module이 될 것이다.
+ * 인스타그램을 만들면 photos 모듈, feed모듈 등등 만들 수 있을 것이다.
+ * 컨트롤러가 하는 일은 기본적으로 url을 가져오고 함수를 실행하는 것.(라우터 같은 존재)
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
